a0d52a10986907e2e621a151eff5cb2f522395c5
golimghostpy <kzap180371@yandex.ru>
2024-12-18 01:04:08 -0500

    muter fixed

--- server/command_handler.cpp
+++ server/command_handler.cpp
@@ -249,20 +249,22 @@ void serve_client(int clientSocket, const char* clientIP, pqxx::work& db) {
     ++cntThreads;
 
     while (true) {
-        lock_guard<mutex> lock(Muter);
-        vector<char> clientBuffer(1024);
+        {
+            lock_guard<mutex> lock(Muter);
+            vector<char> clientBuffer(1024);
 
-        ssize_t bytesRead = recv(clientSocket, clientBuffer.data(), clientBuffer.size() - 1, 0);
-        if (bytesRead <= 0) {
-            cout << "Client [" << clientIP << "] was disconnected" << endl;
-            break;
+            ssize_t bytesRead = recv(clientSocket, clientBuffer.data(), clientBuffer.size() - 1, 0);
+            if (bytesRead <= 0) {
+                cout << "Client [" << clientIP << "] was disconnected" << endl;
+                break;
+            }
+            clientBuffer[bytesRead] = '\0';
+
+            string request(clientBuffer.data());
+
+            string answer = handle_command(db, request);
+            send(clientSocket, answer.c_str(), answer.size(), 0);
         }
-        clientBuffer[bytesRead] = '\0';
-
-        string request(clientBuffer.data());
-
-        string answer = handle_command(db, request);
-        send(clientSocket, answer.c_str(), answer.size(), 0);
     }
 
     close(clientSocket);
