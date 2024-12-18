2b59958baae877b965fac2d2b07e5bc3b12ece9f
golimghostpy <kzap180371@yandex.ru>
2024-12-17 19:32:48 -0500

    sleep removed

--- server/command_handler.cpp
+++ server/command_handler.cpp
@@ -252,7 +252,6 @@ void serve_client(int clientSocket, const char* clientIP, pqxx::work& db) {
         lock_guard<mutex> lock(Muter);
         vector<char> clientBuffer(1024);
 
-        sleep(1);
         ssize_t bytesRead = recv(clientSocket, clientBuffer.data(), clientBuffer.size() - 1, 0);
         if (bytesRead <= 0) {
             cout << "Client [" << clientIP << "] was disconnected" << endl;
